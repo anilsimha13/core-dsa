d77e0274896d64fd9f81e94eb47ab1ca179daad1
anilsimha13 <tanilsimha@gmail.com>
2025-10-11 23:23:44 +0530

    Day-01: Add Programming101.js with basic JavaScript examples

---
+++ Day-01/Programming101.js
@@ -0,0 +1,46 @@
+console.log("~~~Console log~~~~");
+console.log("Hello World!");
+console.log(7);
+console.log(true);
+console.log("~~~Arithmetic Operations~~~");
+const a = 10;
+const b = 100;
+const sum = a + b;
+console.log(sum);
+const subtraction = b - a;
+console.log(subtraction);
+const multiplication = a * b;
+console.log(multiplication);
+const division = b / a;
+console.log(division);
+const modulus = b % a;
+console.log(modulus);
+console.log("~~~String Operations~~~");
+const firstName = "Anil Simha";
+const lastName = "Tallapragada";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log("~~~~Arrays~~~~");
+let arr = [200, 300, 400, 500];
+let sumOfArray = arr[0] + arr[1] + arr[2] + arr[3];
+console.log(sumOfArray);
+let fruits = ["Apple", "Banana", "Orange", "Grapes"];
+console.log(fruits[2]);
+let arayOfArrays = [
+  [100, 200],
+  [300, 400],
+  [500, 600],
+];
+console.log(arayOfArrays[1][0]);
+console.log("~~~~Objects~~~~");
+
+let obj = {
+  name: "Anil Simha Tallapragada",
+  nickName: "Bobby",
+  age: 35,
+  isStudent: true,
+  favoriteColors: ["Red", "Green", "Blue"],
+};
+console.log(obj.favoriteColors[1]);
+console.log(obj.name + " " + obj.nickName);
+console.log("~~~~Functions~~~~");
